93a870470cc70ff9bc1daeff06355cbfc71452ed
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-22 12:29:39 +0100

    Book view

--- routes/searchBooks.js
+++ routes/searchBooks.js
@@ -8,12 +8,25 @@ router.get("/search", async (req, res, next) => {
         const axiosCall = await axios(
           `https://www.googleapis.com/books/v1/volumes?q=${req.query.title}&maxResults=12`
         );
-        const booksInfo = axiosCall.data.items; //esto es un array
+        const booksInfo = axiosCall.data.items; 
         // console.log(booksInfo)
         res.render("books/search.hbs", {booksInfo});
       } catch (err) {
         console.log(err);
     }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const axiosCall = await axios(
+      `https://www.googleapis.com/books/v1/volumes/${req.params.id}`
+    );
+    const bookInfo = axiosCall.data; 
+    // console.log(bookInfo)
+    res.render("books/book.hbs", bookInfo);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
