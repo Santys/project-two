2bb865862aa10c2948bf13229c3295f7976c4a21
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-23 18:33:20 +0100

    Show best books in home page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 
+// Import model
+const Book = require("../models/Book.model")
+
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", async (req, res, next) => {
+  try{
+    const bestBooks = await Book.find()
+    .sort({rating: -1})
+    .limit(10)
+
+    res.render("index", { bestBooks });
+  } catch(err){
+
+  }
 });
 
 module.exports = router;
